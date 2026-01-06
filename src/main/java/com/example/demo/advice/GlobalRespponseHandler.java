4211d303dd4c51affe3fc6f9a17692daee40bc42
Ganesh Lonare <ganeshlonare311@gmail.com>
2026-01-06 09:35:41 +0530

    Add prometheus and grafana monitoring

--- src/main/java/com/example/demo/advice/GlobalRespponseHandler.java
+++ src/main/java/com/example/demo/advice/GlobalRespponseHandler.java
@@ -10,14 +10,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
 public class GlobalRespponseHandler implements ResponseBodyAdvice<Object> {
+
     @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        return true;
+    public boolean supports(MethodParameter returnType,
+                            Class<? extends HttpMessageConverter<?>> converterType) {
+
+        // ❌ Do not apply to actuator endpoints
+        return !returnType.getDeclaringClass()
+                .getPackageName()
+                .startsWith("org.springframework.boot.actuate");
     }
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if(body instanceof ApiResponse<?>){
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+
+        if (body instanceof ApiResponse<?>) {
             return body;
         }
         return new ApiResponse<>(body);
